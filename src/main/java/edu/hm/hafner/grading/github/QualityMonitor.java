3ea16633c7dd8ef15b394368338245c0f86cb72a
Ulli Hafner <ullrich.hafner@gmail.com>
2024-02-29 17:55:40 +0100

    Make action independent of github-autograding.

--- src/main/java/edu/hm/hafner/grading/github/QualityMonitor.java
+++ src/main/java/edu/hm/hafner/grading/github/QualityMonitor.java
@@ -1,23 +1,42 @@
 package edu.hm.hafner.grading.github;
 
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Date;
 
+import org.apache.commons.lang3.StringUtils;
+
+import edu.hm.hafner.grading.AggregatedScore;
+import edu.hm.hafner.grading.AutoGradingRunner;
+import edu.hm.hafner.grading.GradingReport;
+import edu.hm.hafner.util.FilteredLog;
 import edu.hm.hafner.util.VisibleForTesting;
+
+import org.kohsuke.github.GHCheckRun;
+import org.kohsuke.github.GHCheckRun.Conclusion;
+import org.kohsuke.github.GHCheckRun.Status;
+import org.kohsuke.github.GHCheckRunBuilder;
+import org.kohsuke.github.GHCheckRunBuilder.Output;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
 
 /**
  * GitHub action entrypoint for the quality monitor action.
  *
  * @author Ullrich Hafner
  */
-public class QualityMonitor extends GitHubAutoGradingRunner {
+public class QualityMonitor extends AutoGradingRunner {
     static final String QUALITY_MONITOR = "Quality Monitor";
 
     /**
-     * Public entry point for the GitHub action in the docker container, simply calls the action.
-     *
-     * @param unused
-     *         not used
-     */
+         * Public entry point for the GitHub action in the docker container, simply calls the action.
+         *
+         * @param unused
+         *         not used
+         */
     public static void main(final String... unused) {
         new QualityMonitor().run();
     }
@@ -42,5 +61,118 @@ public class QualityMonitor extends GitHubAutoGradingRunner {
     @Override
     protected String getDefaultConfigurationPath() {
         return "/default-no-score-config.json";
+    }
+
+    @Override
+    protected void publishGradingResult(final AggregatedScore score, final FilteredLog log) {
+        var errors = createErrorMessageMarkdown(log);
+
+        var results = new GradingReport();
+        addComment(score,
+                results.getTextSummary(score, getChecksName()),
+                results.getMarkdownDetails(score, getChecksName()) + errors,
+                results.getSubScoreDetails(score) + errors,
+                results.getMarkdownSummary(score, getChecksName()) + errors,
+                errors.isBlank() ? Conclusion.SUCCESS : Conclusion.FAILURE, log);
+
+        try {
+            var environmentVariables = createEnvironmentVariables(score, log);
+            Files.writeString(Paths.get("metrics.env"), environmentVariables);
+        }
+        catch (IOException exception) {
+            log.logException(exception, "Can't write environment variables to 'metrics.env'");
+        }
+
+        log.logInfo("GitHub Action has finished");
+    }
+
+    @Override
+    protected void publishError(final AggregatedScore score, final FilteredLog log, final Throwable exception) {
+        var results = new GradingReport();
+
+        var markdownErrors = results.getMarkdownErrors(score, exception);
+        addComment(score, results.getTextSummary(score, getChecksName()),
+                markdownErrors, markdownErrors, markdownErrors, Conclusion.FAILURE, log);
+    }
+
+    private void addComment(final AggregatedScore score, final String textSummary,
+            final String markdownDetails, final String markdownSummary, final String prSummary,
+            final Conclusion conclusion, final FilteredLog log) {
+        try {
+            var repository = getEnv("GITHUB_REPOSITORY", log);
+            if (repository.isBlank()) {
+                log.logError("No GITHUB_REPOSITORY defined - skipping");
+
+                return;
+            }
+            String oAuthToken = getEnv("GITHUB_TOKEN", log);
+            if (oAuthToken.isBlank()) {
+                log.logError("No valid GITHUB_TOKEN found - skipping");
+
+                return;
+            }
+
+            String sha = getEnv("GITHUB_SHA", log);
+
+            GitHub github = new GitHubBuilder().withAppInstallationToken(oAuthToken).build();
+            GHCheckRunBuilder check = github.getRepository(repository)
+                    .createCheckRun(getChecksName(), sha)
+                    .withStatus(Status.COMPLETED)
+                    .withStartedAt(Date.from(Instant.now()))
+                    .withConclusion(conclusion);
+
+            Output output = new Output(textSummary, markdownSummary).withText(markdownDetails);
+
+            if (getEnv("SKIP_ANNOTATIONS", log).isEmpty()) {
+                var annotationBuilder = new GitHubAnnotationsBuilder(output, computeAbsolutePathPrefixToRemove(log));
+                annotationBuilder.createAnnotations(score);
+            }
+
+            check.add(output);
+
+            GHCheckRun run = check.create();
+            log.logInfo("Successfully created check " + run);
+
+            var prNumber = getEnv("PR_NUMBER", log);
+            if (!prNumber.isBlank()) { // optional PR comment
+                github.getRepository(repository)
+                        .getPullRequest(Integer.parseInt(prNumber))
+                        .comment(prSummary + addCheckLink(run));
+                log.logInfo("Successfully commented PR#" + prNumber);
+            }
+        }
+        catch (IOException exception) {
+            log.logException(exception, "Could not create check");
+        }
+    }
+
+    String createEnvironmentVariables(final AggregatedScore score, final FilteredLog log) {
+        var metrics = new StringBuilder();
+        score.getMetrics().forEach((metric, value) -> metrics.append(String.format("%s=%d%n", metric, value)));
+        log.logInfo("---------------");
+        log.logInfo("Metrics Summary");
+        log.logInfo("---------------");
+        log.logInfo(metrics.toString());
+        return metrics.toString();
+    }
+
+    private String getChecksName() {
+        return StringUtils.defaultIfBlank(System.getenv("CHECKS_NAME"), getDisplayName());
+    }
+
+    private String computeAbsolutePathPrefixToRemove(final FilteredLog log) {
+        return String.format("%s/%s/", getEnv("RUNNER_WORKSPACE", log),
+                StringUtils.substringAfter(getEnv("GITHUB_REPOSITORY", log), "/"));
+    }
+
+    private String addCheckLink(final GHCheckRun run) {
+        return String.format("%n%n More details are available in the [GitHub Checks Result](%s).%n",
+                run.getDetailsUrl().toString());
+    }
+
+    private String getEnv(final String key, final FilteredLog log) {
+        String value = StringUtils.defaultString(System.getenv(key));
+        log.logInfo(">>>> " + key + ": " + value);
+        return value;
     }
 }
