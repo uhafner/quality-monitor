fad4e74c8db290128a74f06e72eae2c17dd97863
Ulli Hafner <ullrich.hafner@gmail.com>
2026-04-22 22:17:30 +0200

    Bump version to next development version 4.12.0-SNAPSHOT

--- src/test/java/edu/hm/hafner/grading/github/QualityMonitorDockerITest.java
+++ src/test/java/edu/hm/hafner/grading/github/QualityMonitorDockerITest.java
@@ -239,7 +239,7 @@ class QualityMonitorDockerITest {
     }
 
     private GenericContainer<?> createContainer() {
-        return new GenericContainer<>(DockerImageName.parse("uhafner/quality-monitor:4.11.0"));
+        return new GenericContainer<>(DockerImageName.parse("uhafner/quality-monitor:4.12.0-SNAPSHOT"));
     }
 
     private String readStandardOut(final GenericContainer<? extends GenericContainer<?>> container)
